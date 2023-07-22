c3a3387ba202794514b73a3fa7ea51d98a449686
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-23 00:33:40 +0100

    more simpe UI

--- client/src/Pages/Auth.js
+++ client/src/Pages/Auth.js
@@ -9,13 +9,36 @@ const Auth = () => {
   )
 }
 
-const Login = () => {
+const Register = () => {
   return(
-    <div>
-      
+    <div className="left">
+      <div className="contact">
+        <form action="">
+          <h3>Register</h3>
+          <input type="text" placeholder="USERNAME" />
+          <input type="text" placeholder="PASSWORD" />
+          <button className="submit">REGISTER</button>
+        </form>
+      </div>
     </div>
   )
 }
+
+const Login = () => {
+  return(
+    <div className="left">
+      <div className="contact">
+        <form action="">
+          <h3>Login</h3>
+          <input type="text" placeholder="USERNAME" />
+          <input type="text" placeholder="PASSWORD" />
+          <button className="submit">login</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
 
 
 export default Auth;
