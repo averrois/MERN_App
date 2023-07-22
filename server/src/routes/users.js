a7febd6e43dc1d5396d51b56cb8e2eff3642bed8
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-22 22:42:31 +0100

    create register&login backend logic

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -11,9 +11,39 @@ router.post("/register", async (req, res) => {
 
     const user = await UserModel.findOne({username});
 
-    res.json(user);
+    if(user){
+        return res.json({message: "User already exists!"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({username, password: hashedPassword});
+
+    // Similar to Laravel
+    await newUser.save();
+
+    res.json({message: "The User Registerd successfully!"});
 });
-router.post("login");
+router.post("/login", async (req, res) => {
+    const {username, password} = req.body;
+
+    const user = await UserModel.findOne({username});
+
+    if (!user) {
+        return res.json({message: "User deosn't Exist!"});
+    }
+    
+    // Check if the ps is correct
+    const isPsCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPsCorrect) {
+        return res.json({message: "Username or Password is Incorrect!"});
+    }
+
+    const token = jwt.sign({id : user._id}, "secret");
+    res.json({token, userID: user._id});
+
+});
 
 
 export {router as userRouter};
