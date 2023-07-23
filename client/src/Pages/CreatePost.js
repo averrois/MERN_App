08a4eff52cebadd0d42865d5e939181e131369f7
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-23 20:27:03 +0100

    post that is created does not inserted to db

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function CreatePost() {
   const [post, setPost] = useState({
@@ -9,85 +10,89 @@ function CreatePost() {
     userOwner: 0
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-  
-    // If the input is for tags, split the comma-separated string into an array
-    if (name === "tags") {
-      const tagsArray = value.split(",");
-      setPost({ ...post, [name]: tagsArray });
-    } else {
-      // Otherwise, update the state normally
-      setPost({ ...post, [name]: value });
-    }
-  };
-  
-
   const addTag = (e) => {
     e.preventDefault();
-    setPost({ ...post, tags: [...post.tags, ""] });
-  };  
+    setPost({ ...post, tags: [...post.tags, post.tagsInput] });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/posts", post);
+      alert("Post Created!");
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className='post'>
       <div className="post-container">
-          <form>
-              <h3>Create Post</h3>
-              <div className='row title_inp'>
-                <label htmlFor='title'>title</label>
+        <form onSubmit={onSubmit}>
+          <h3>Create Post</h3>
+          <div className='row title_inp'>
+            <label htmlFor='title'>title</label>
+            <input 
+              type="text" 
+              placeholder="Title Of Post"
+              name='title' 
+              onChange={(e) => setPost({ ...post, title: e.target.value })}
+            />
+          </div>
+          <div className='row'>
+            <label htmlFor='title'>Content</label>
+            <textarea 
+              type="text" 
+              placeholder="Content Of Post"
+              name='content' 
+              onChange={(e) => setPost({ ...post, content: e.target.value })}
+            />
+          </div>
+          <div className='row'>
+            <label htmlFor='title'>Tags</label>
+            <div className='tags'>
+              <div className='tags-row'>
                 <input 
                   type="text" 
-                  placeholder="Title Of Post"
-                  name='title' 
-                  onChange={handleChange}
+                  placeholder="Tags..."
+                  name='tags' 
+                  onChange={(e) => {
+                      setPost({ ...post, tagsInput: e.target.value });
+                    } 
+                  }
                 />
+                <button 
+                  onClick={addTag}
+                  type='button'
+                >
+                  +
+                </button>
               </div>
-              <div className='row'>
-                <label htmlFor='title'>Content</label>
-                <textarea 
-                  type="text" 
-                  placeholder="Content Of Post"
-                  name='content' 
-                  onChange={handleChange} 
-                />
+              <div className='tags-row'>
+                {post.tags.map((tag, index) => (
+                  <span key={index}>{tag}</span>
+                ))}
               </div>
-              <div className='row'>
-                <label htmlFor='title'>Tags</label>
-                <div className='tags'>
-                  <div className='tags-row'>
-                    <input 
-                      type="text" 
-                      placeholder="Tags..."
-                      name='tags' 
-                      onChange={handleChange}// Join the tags array into a comma-separated string
-                    />
-                    <button onClick={addTag}>+</button>
-                  </div>
-                  <div className='tags-row'>
-                    {post.tags.map((tag, index) => (
-                      <span key={index}>{tag}</span>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className='row'>
-                <label htmlFor='title'>Image Url</label>
-                <input 
-                  type="text" 
-                  placeholder="Image URL"
-                  name='imgUrl' 
-                  onChange={handleChange}
-                />
-              </div>
-              <div className='row'>
-                <button>
-                  Create Post
-                </button> 
-              </div>
-          </form>
+            </div>
+          </div>
+          <div className='row'>
+            <label htmlFor='title'>Image Url</label>
+            <input 
+              type="text" 
+              placeholder="Image URL"
+              name='imgUrl' 
+              onChange={(e) => setPost({ ...post, imgUrl: e.target.value })}
+            />
+          </div>
+          <div className='row'>
+            <button type='submit'>
+              Create Post
+            </button> 
+          </div>
+        </form>
       </div>
     </div>
   )
 }
 
-export default CreatePost
+export default CreatePost;
