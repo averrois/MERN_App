edbc49e33a9c1861768317c95ba5ac077df39758
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-24 18:20:29 +0100

    creating the Home UI

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,8 +1,38 @@
-import React from 'react'
+// import React, {useEffect, useState} from 'react';
+// import axios from 'axios';
 
 function Home() {
+  // const [post, setPost] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchPost = async () => {
+  //     try {
+  //       const response = await axios.get("http://localhost:3001/posts", post);
+  //       setPost(response.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   fetchPost();
+
+  // });
   return (
-    <div>Home</div>
+    <div className='browser'>
+      <ul>
+        <li>
+          <h1>Title</h1>
+        </li>
+        <li>
+          <span>Tags</span>
+        </li>
+        <li>
+          <p>Lorem LoremLoremLoremLoremLoremLoremLoremLoremLorem LoremLoremLoremLoremLorem
+          LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem
+          </p>
+        </li>
+      </ul>
+    </div>
   )
 }
 
