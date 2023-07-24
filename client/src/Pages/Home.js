4de677a0f5c3f31ac0ac9e72bf5eca6d8578db1f
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-24 21:23:32 +0100

    Saved Posts UI

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,28 +1,53 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {useGetUserID} from '../hooks/useGetUserID';
 
 function Home() {
+  const userID = useGetUserID();
   const [posts, setPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
 
   useEffect(() => {
     const fetchPost = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/posts", posts);
+        const response = await axios.get("http://localhost:3001/posts");
         setPosts(response.data);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchPost();
+    const fetchSavedPost = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/posts/savedPosts/ids/${userID}`);
+        setSavedPosts(response.data.savedPosts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  });
+    fetchPost();
+    fetchSavedPost();
+
+  }, [userID]);
+
+  const savePost = async (postID) => {
+    try {
+      const response = await axios.put("http://localhost:3001/posts", {postID, userID});
+      setSavedPosts(response.data.savedPosts);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const isPostSaved = (id) => savedPosts.includes(id);
+
   return (
     <div className='browser'>
       <h1 className='title'>All Posts ...</h1>
       {posts.map((post) => (
         
-        <div className='article'>
+        <div className='article' key={post._id}>
           <ul>
             <li>
               <h1>{post.title}</h1>
@@ -41,10 +66,12 @@ function Home() {
               </p>
             </li>
             <li>
-              <button
+              <button 
+                onClick={() => savePost(post._id)} 
                 type='button'
+                disabled={isPostSaved(post._id)}
               >
-                Save
+                {isPostSaved(post._id) ? "unsave" : "save"}
               </button>
             </li>
           </ul>
--- client/src/Pages/SavedPost.js
+++ client/src/Pages/SavedPost.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {useGetUserID} from '../hooks/useGetUserID';
 
 function SavedPost() {
+  const userID = useGetUserID();
+  const [savedPosts, setSavedPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchSavedPost = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/posts/savedPosts/${userID}`);
+        setSavedPosts(response.data.savedPosts);
+        console.log(response.data.savedPosts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchSavedPost();
+
+  }, [userID]);
+
   return (
-    <div>SavedPost</div>
+    <div className='browser'>
+      <h1 className='title'>Saved Posts ...</h1>
+      {savedPosts.map((post) => (
+        
+        <div className='article' key={post._id}>
+          <ul>
+            <li>
+              <h1>{post.title}</h1>
+            </li>
+            <li>
+              <span>Tags</span>
+            </li>
+            <li>
+              <div className="img_container">
+                <img src={post.imgUrl} alt={post.title}/>
+              </div>
+            </li>
+            <li>
+              <p>
+                {post.content}
+              </p>
+            </li>
+          </ul>
+        </div>
+      ))
+    }     
+    </div>
   )
 }
 
-export default SavedPost
+export default SavedPost;
