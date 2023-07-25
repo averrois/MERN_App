9ca4c04b85c58a8651490a14e75af2bb7f9b72f5
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-25 20:32:59 +0100

    Create auth Middleware

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -47,3 +47,16 @@ router.post("/login", async (req, res) => {
 
 
 export {router as userRouter};
+
+// Creating Middleware
+export const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, "secret", (err) => {
+            if (err) return res.sendStatus(403);
+            next();
+        })
+    } else {
+        res.sendStatus(401);
+    }
+}
